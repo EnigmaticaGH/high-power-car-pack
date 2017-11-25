81c5691860d74f11483f2f4013cb61a95fc5efd5
EnigmaticaGH <eventhorizonsoundtracks@gmail.com>
2017-11-24 20:44:57 -0500

    Initial Commit
    Initial commit

---
+++ vehicles/common/tires/sb_tires/materials.cs
@@ -0,0 +1,51 @@
+singleton Material(tire_02c)
+{
+    mapTo = "tire_02c";
+    diffuseMap[1] = "vehicles/common/tires/sb_tires/tire_02c_d.dds";
+    specularMap[1] = "vehicles/common/tires/tire_01d_s.dds";
+    normalMap[1] = "vehicles/common/tires/tire_01d_n.dds";
+    diffuseMap[0] = "vehicles/common/null.dds";
+    specularMap[0] = "vehicles/common/null.dds";
+    normalMap[0] = "vehicles/common/tires/tire_01d_n.dds";
+    specularPower[0] = "32";
+    pixelSpecular[0] = "1";
+    specularPower[1] = "32";
+    pixelSpecular[1] = "1";
+    diffuseColor[0] = "1 1 1 1";
+    diffuseColor[1] = "1 1 1 1.1";
+    useAnisotropic[0] = "1";
+    useAnisotropic[1] = "1";
+    castShadows = "1";
+    translucent = "1";
+    translucentBlendOp = "None";
+    alphaTest = "0";
+    alphaRef = "0";
+    dynamicCubemap = true; //cubemap = "BNG_Sky_02_cubemap";
+    materialTag0 = "beamng"; materialTag1 = "vehicle";
+};
+
+singleton Material(tire_03a)
+{
+    mapTo = "tire_03a";
+    diffuseMap[1] = "vehicles/common/tires/sb_tires/tire_03a_d.dds";
+    specularMap[1] = "vehicles/common/tires/tire_01d_s.dds";
+    normalMap[1] = "vehicles/common/tires/tire_01d_n.dds";
+    diffuseMap[0] = "vehicles/common/null.dds";
+    specularMap[0] = "vehicles/common/null.dds";
+    normalMap[0] = "vehicles/common/tires/tire_01d_n.dds";
+    specularPower[0] = "32";
+    pixelSpecular[0] = "1";
+    specularPower[1] = "32";
+    pixelSpecular[1] = "1";
+    diffuseColor[0] = "1 1 1 1";
+    diffuseColor[1] = "1 1 1 1.1";
+    useAnisotropic[0] = "1";
+    useAnisotropic[1] = "1";
+    castShadows = "1";
+    translucent = "1";
+    translucentBlendOp = "None";
+    alphaTest = "0";
+    alphaRef = "0";
+    dynamicCubemap = true; //cubemap = "BNG_Sky_02_cubemap";
+    materialTag0 = "beamng"; materialTag1 = "vehicle";
+};
